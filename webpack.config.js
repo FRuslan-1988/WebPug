658bc328fca9bc67c584024cef70d1abd81a2eee
Ruslan <rfattyahov@gmail.com>
2024-02-16 12:01:09 +0300

    стартовая верстка

---
+++ webpack.config.js
@@ -0,0 +1,132 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+
+const devMode = mode === "development";
+const target = devMode ? "web" : "browserslist";
+const devtool = devMode ? "source-map" : undefined;
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        open: true,
+    },
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+        filename: "index.[contenthash].js",
+        // assetModuleFilename: "assets/[hash][ext]"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", 'index.pug'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: "index.[contenthash].css",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+              test: /\.pug$/,
+              loader: "pug-loader",
+            },
+            // {
+            //     test: /\.html$/i,
+            //     loader: "html-loader",
+            // },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [require("postcss-preset-env")],
+                            }
+                        }
+                    },
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: "images/[name][ext]"
+                }
+            },
+            
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "fonts/[name][ext]"
+                }
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/i,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+            },
+        ]
+    },
+    optimization: {
+        minimizer: [
+          "...",
+          new ImageMinimizerPlugin({
+            minimizer: {
+              implementation: ImageMinimizerPlugin.imageminMinify,
+              options: {
+                // Lossless optimization with custom option
+                // Feel free to experiment with options for better result for you
+                plugins: [
+                  ["gifsicle", { interlaced: true }],
+                  ["jpegtran", { progressive: true }],
+                  ["optipng", { optimizationLevel: 5 }],
+                  // Svgo configuration here https://github.com/svg/svgo#configuration
+                  [
+                    "svgo",
+                    {
+                      plugins: [
+                        {
+                          name: "preset-default",
+                          params: {
+                            overrides: {
+                              removeViewBox: false,
+                              addAttributesToSVGElement: {
+                                params: {
+                                  attributes: [
+                                    { xmlns: "http://www.w3.org/2000/svg" },
+                                  ],
+                                },
+                              },
+                            },
+                          },
+                        },
+                      ],
+                    },
+                  ],
+                ],
+              },
+            },
+          }),
+        ],
+      },
+    
+};
